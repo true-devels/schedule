d94b05e2ca4eee300fad8d6e90438f93479adaea
Andrii Fedorko <andriy.fedorko01@gmail.com>
2018-07-24 14:46:55 +0300

    add startActivityForResult

---
+++ app/src/main/java/com/company/schedule/AddNoteActivity.java
@@ -0,0 +1,47 @@
+package com.company.schedule;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddNoteActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText etNameNote;
+    Button btnSubmitNote;
+    final String TAG = "myLog AddNoteActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_note);
+
+        etNameNote = (EditText) findViewById(R.id.etNameNote);
+        btnSubmitNote = (Button) findViewById(R.id.btnSubmitNoteName);
+        btnSubmitNote.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnSubmitNoteName:
+                Intent intent = new Intent();
+                String noteName = etNameNote.getText().toString();
+
+                if (noteName.isEmpty()){
+                    setResult(RESULT_CANCELED);
+                    Log.d(TAG, "RESULT_CANCELED, noteName: \"" + noteName + "\";");
+                }
+                else { // if noteName is not empty
+                    intent.putExtra("note_name", noteName);
+
+                    setResult(RESULT_OK, intent);
+                    Log.d(TAG, "RESULT_OK, noteName: \"" + noteName + "\";");
+                }
+                finish();
+        }
+    }
+}
--- app/src/main/java/com/company/schedule/MainActivity.java
+++ app/src/main/java/com/company/schedule/MainActivity.java
@@ -1,15 +1,23 @@
 package com.company.schedule;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView testOutputNoteName;// TODO delete the testOutputNoteName
+//  when we move to a new activity to get the result we need to be indexed by its number
+    final int REQUEST_CODE_ADD_NOTE = 1;
+    final String TAG = "myLog MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +26,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        testOutputNoteName = (TextView) findViewById(R.id.testOutputNoteName);  // test TV for test result
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(this);
     }
 
     @Override
@@ -48,5 +52,48 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+        case R.id.fab:
+            // we indicate an explicit transition to AddNoteActivity to enter the data of a note
+            Intent intent = new Intent(MainActivity.this, AddNoteActivity.class);
+            // and getting this information back. (using REQUEST_CODE_ADD_NOTE (1) we can find out that the result came exactly with AddNoteActivity)
+            startActivityForResult(intent, REQUEST_CODE_ADD_NOTE);
+            break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        on requestCode we determine from which subsidiary activity the result came
+//        resultCode - return code. Determines whether the call has passed successfully or not.
+//        data - Intent, in which the data is returned
+
+        // if data was correct entered
+        if (resultCode == RESULT_OK && data != null) {
+            Log.d(TAG, "RESULT_OK");
+            switch (requestCode) {  // check from which daughter object data come
+            case REQUEST_CODE_ADD_NOTE:  // if from AddNoteActivity
+                String noteName = data.getStringExtra("note_name");
+                Log.d(TAG, "case REQUEST_CODE_ADD_NOTE, noteName: \"" + noteName + "\";");
+                //TODO delete cod below
+                testOutputNoteName.setText(noteName);
+                //TODO to DB element with name noteName
+                break;
+            default:
+                Log.d(TAG, "default");
+            }
+        } else if (requestCode == RESULT_CANCELED) {
+            // TODO output error
+            Log.d(TAG, "RESULT_CANCELED");
+        }
+        else {
+            //TODO output error
+            Log.d(TAG, "data == null or any different error, requestCode: \"" + requestCode + "\";");
+        }
     }
 }
